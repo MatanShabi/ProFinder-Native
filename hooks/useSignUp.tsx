59cbf5069d6103359d42bee3dcfca53371a0a85d
Raz Hagay Gavriel <raz5338@gmail.com>
2024-06-19 15:40:38 +0300

    Add profile picture upload functionality to signup process

--- hooks/useSignUp.tsx
+++ hooks/useSignUp.tsx
@@ -5,7 +5,10 @@ import {
   createUserWithEmailAndPassword,
   updateProfile,
 } from "firebase/auth";
-import { auth } from "../config/firebase";
+import { auth, storage } from "../config/firebase";
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
+
+import axios from "axios"; 
 
 const useSignUp = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -14,10 +17,14 @@ const useSignUp = () => {
     UserCredential | undefined
   >(undefined);
 
-  const updateUserProfile = async (user: User, firstName: string, lastName: string) => {
+  const updateUserProfile = async (user: User, 
+     firstName: string,
+     lastName: string,
+     photoUrl: string ) => {
     if (user) {
       await updateProfile(user, {
         displayName: `${firstName} ${lastName}`,
+        photoURL: photoUrl
       });
     }
   };
@@ -31,9 +38,23 @@ const useSignUp = () => {
     setIsLoading(true);
     setIsError(false);
     try {
+      let apiUrl = 'https://ui-avatars.com/api/'
+      apiUrl += `?name=${encodeURIComponent(firstName + " " + lastName)}&size=200&color=fff&rounded=true`
+      const randomBackgroundColor = generateRandomColor();
+      apiUrl += `&background=${encodeURIComponent(randomBackgroundColor)}`;
+      const response = await axios.get(apiUrl, { responseType: 'blob' });
+      const pictureBlob = await response.data;
+
+      const storageRef = ref(storage, `profilePictures/${email}.png`);
+
+      await uploadBytes(storageRef, pictureBlob)
+      
+      const downloadURL = await getDownloadURL(storageRef);
+
       const user = await createUserWithEmailAndPassword(auth, email, password);
-      await updateUserProfile(user.user, firstName, lastName);
+      await updateUserProfile(user.user, firstName, lastName, downloadURL);
       setUserCredentials(user)
+
     } catch (error) {
       setIsError(true);
       console.error("Error creating user: ", error);
@@ -44,5 +65,10 @@ const useSignUp = () => {
 
   return { createUser, updateUserProfile, userCredentials, isLoading, isError };
 };
+
+function generateRandomColor(): string {
+  const random = Math.floor(Math.random() * 16777215).toString(16);
+  return '#' + '0'.repeat(6 - random.length) + random;
+}
 
 export default useSignUp;
