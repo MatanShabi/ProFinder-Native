47267f54e6642285095f2e345dc6a5d5161123a5
Raz Hagay Gavriel <raz5338@gmail.com>
2024-06-20 19:19:22 +0300

    Add loading indicator to login and signup screens

--- app/(screens)/login.tsx
+++ app/(screens)/login.tsx
@@ -1,13 +1,14 @@
 import { FC, useEffect } from "react";
 import Header from "@/components/Header";
-import { StyleSheet, View } from "react-native";
-import { Button, Text } from "react-native-paper";
+import { StyleSheet, View, Modal } from "react-native";
+import { Button, Text, ActivityIndicator } from "react-native-paper";
 import { ThemedView } from "@/components/ThemedView";
 import { StackNavigationProp } from "@react-navigation/stack";
 import NavigationLink from "@/components/navigation/NavigationLink";
 import { HomePageStackParamList } from "@/components/navigation/HomePageNavigation";
 import useLogin from "@/hooks/useLogin";
 import { FormProvider, useForm } from "react-hook-form";
+
 import FormInput from "@/components/FormInput";
 
 type LoginScreenProps = {
@@ -61,14 +62,34 @@ const LoginScreen: FC<LoginScreenProps> = ({ navigation }) => {
                         Login
                     </Button>
                 </View>
+                {isLoading && (
+                    <Modal transparent={false} animationType="none">
+                        <View style={styles.loadingContainer}>
+                            <ActivityIndicator animating={true} size={120} />
+                        </View>
+                    </Modal>
+                )}
             </ThemedView>
         </FormProvider>
     );
 };
 
 const styles = StyleSheet.create({
-    container: { display: "flex", gap: 15, margin: 20, marginTop: 40 },
-    containerHeader: { fontWeight: "700", marginBottom: 10 },
+    container: {
+        display: "flex",
+        gap: 15,
+        margin: 20,
+        marginTop: 40
+    },
+    containerHeader: {
+        fontWeight: "700",
+        marginBottom: 10
+    },
+    loadingContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    }
 });
 
 export default LoginScreen;
