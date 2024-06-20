3cf7a456bacb1ba52598a7fe56e62231277398d8
Raz Hagay Gavriel <raz5338@gmail.com>
2024-06-20 05:46:20 +0300

    fix bug in error screen

--- app/(screens)/logout.tsx
+++ app/(screens)/logout.tsx
@@ -1,16 +1,24 @@
 import React from "react";
 import { ThemedView } from "@/components/ThemedView";
+import { ActivityIndicator } from "react-native-paper";
 import useLogout from "@/hooks/useLogout";
+import ErrorNotification from "@/components/ErrorNotification";
 
 const LogoutScreen = () => {
     const { showAlert, isLoading, isError, error } = useLogout();
 
     if (isLoading) {
-        return <ThemedView>Loading...</ThemedView>;
+        return (<ThemedView>
+            <ActivityIndicator animating={true} size="large" />
+        </ThemedView>);
     }
 
     if (isError && error) {
-        return <ThemedView>Error: {error}</ThemedView>;
+        return (<ThemedView>
+            <ErrorNotification error={error} onClose={() => { }} />
+        </ThemedView>
+
+        )
     }
 
     return (
