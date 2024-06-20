b91d957c3900f2bec03f3daade54f44dc5d1e6e7
Raz Hagay Gavriel <raz5338@gmail.com>
2024-06-20 20:21:04 +0300

    Refactor handlePictureAPI to improve maitance

--- scripts/handlePictureAPI.ts
+++ scripts/handlePictureAPI.ts
@@ -5,14 +5,19 @@ function generateRandomColor(): string {
   return '#' + '0'.repeat(6 - random.length) + random;
 }
 
-export const handlePictureAPI = async (
-  firstName: string,
-  lastName: string
-): Promise<Blob> => {
+export const handlePictureAPI = async (firstName: string, lastName: string): Promise<Blob> => {
   let apiUrl = 'https://ui-avatars.com/api/';
-  apiUrl += `?name=${encodeURIComponent(firstName + " " + lastName)}&size=200&color=fff&rounded=true`;
+  const nameParam = `?name=${encodeURIComponent(firstName + " " + lastName)}`;
+  const sizeParam = `&size=200`;
+  const colorParam = `&color=fff`;
+  const roundedParam = `&rounded=true`;
+
+  apiUrl += nameParam + sizeParam + colorParam + roundedParam;
+
   const randomBackgroundColor = generateRandomColor();
-  apiUrl += `&background=${encodeURIComponent(randomBackgroundColor)}`;
+  const backgroundParam = `&background=${encodeURIComponent(randomBackgroundColor)}`;
+
+  apiUrl += backgroundParam;
 
   const response = await axios.get(apiUrl, { responseType: 'blob' });
   return response.data;
