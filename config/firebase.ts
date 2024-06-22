5ce52b55849698641619d1d821f97dd05147fbb4
Raz Hagay Gavriel <raz5338@cs.colman.ac.il>
2024-06-22 13:14:52 +0300

    Delete unused imports

--- config/firebase.ts
+++ config/firebase.ts
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import firestore from "@react-native-firebase/firestore";
 import { getStorage } from "firebase/storage";
 import "@react-native-firebase/storage";
 import { firebaseConfig } from "./env";
@@ -10,7 +9,6 @@ import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
-// export const db = firestore();
 
 export const storage = getStorage(app);
 
