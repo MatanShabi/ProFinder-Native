43c67530189a9acbe627c16c6956e93fffd19f20
Raz Hagay Gavriel <raz5338@cs.colman.ac.il>
2024-06-22 13:09:49 +0300

    Add "New Post" implementation

---
+++ app/(screens)/addpost.tsx
@@ -0,0 +1,149 @@
+import React from "react";
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  ScrollView,
+  Image,
+  ActivityIndicator,
+  Modal,
+} from "react-native";
+import { Button, Text } from "react-native-paper";
+import { useNavigation } from "@react-navigation/native";
+import ErrorNotification from "@/components/ErrorNotification";
+import useAddPost from "../../hooks/useAddPost";
+
+const AddPostScreen = () => {
+  const navigation = useNavigation<any>();
+
+  const {
+    title,
+    description,
+    link,
+    price,
+    image,
+    errors,
+    isLoading,
+    isError,
+    error,
+    setTitle,
+    setDescription,
+    setLink,
+    setPrice,
+    setIsError,
+    handlePickImage,
+    handleSubmit,
+  } = useAddPost(navigation);
+
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <TextInput
+        placeholder="Title"
+        value={title}
+        onChangeText={setTitle}
+        style={styles.input}
+      />
+      {errors.title && <Text style={styles.error}>{errors.title}</Text>}
+      <TextInput
+        placeholder="Description"
+        value={description}
+        onChangeText={setDescription}
+        style={[styles.input, styles.descriptionInput]}
+        multiline
+      />
+      {errors.description && (
+        <Text style={styles.error}>{errors.description}</Text>
+      )}
+      <TextInput
+        placeholder="Link"
+        value={link}
+        onChangeText={setLink}
+        style={styles.input}
+      />
+      {errors.link && <Text style={styles.error}>{errors.link}</Text>}
+      <TextInput
+        placeholder="Price"
+        value={price}
+        onChangeText={setPrice}
+        style={styles.input}
+        keyboardType="numeric"
+      />
+      {errors.price && <Text style={styles.error}>{errors.price}</Text>}
+      <View style={styles.imagePicker}>
+        <Button mode="outlined" onPress={handlePickImage}>
+          {image ? "Change Image" : "Add Image"}
+        </Button>
+        {image && <Image source={{ uri: image }} style={styles.imagePreview} />}
+      </View>
+      {isLoading ? 
+      (
+        <Modal transparent={false} animationType="none">
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator animating={true} size={120} />
+        </View>
+      </Modal>
+      ) : 
+      (
+        <Button
+          mode="contained"
+          onPress={handleSubmit}
+          style={styles.submitButton}
+        >
+          Submit
+        </Button>
+      )}
+      {isError && (
+        <ErrorNotification
+          visible={isError}
+          errorMessage={error || ""}
+          onDismiss={() => {
+            setIsError(false);
+          }}
+        />
+      )}
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    padding: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    marginBottom: 15,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    fontSize: 16,
+  },
+  descriptionInput: {
+    minHeight: 100,
+  },
+  imagePicker: {
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  imagePreview: {
+    marginTop: 10,
+    width: 100,
+    height: 100,
+  },
+  submitButton: {
+    marginTop: 20,
+  },
+  error: {
+    color: "red",
+    marginBottom: 10,
+    textAlign: "center",
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default AddPostScreen;
---
+++ hooks/useAddPost.tsx
@@ -0,0 +1,157 @@
+import { useState, useCallback } from "react";
+import { db, storage } from "@/config/firebase";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import * as ImagePicker from "expo-image-picker";
+import { addDoc, collection } from "firebase/firestore";
+import useUser from "@/hooks/useUser";
+import { NavigationProp } from "@react-navigation/native";
+import { useFocusEffect } from "expo-router/build/useFocusEffect";
+import { generateRandomName } from "../scripts/generateRandomName";
+
+const useAddPost = (navigation: NavigationProp<any>) => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [link, setLink] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState<string | null>(null);
+  const { user } = useUser();
+
+  const [errors, setErrors] = useState({
+    title: "",
+    description: "",
+    link: "",
+    price: "",
+  });
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useFocusEffect(
+    useCallback(() => {
+      return () => {
+        setTitle("");
+        setDescription("");
+        setLink("");
+        setPrice("");
+        setImage(null);
+        setErrors({ title: "", description: "", link: "", price: "" });
+        setIsLoading(false);
+        setIsError(false);
+        setError(null);
+      };
+    }, [])
+  );
+
+  const handlePickImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+
+      if (!result.canceled) {
+        setImage(result.assets[0].uri);
+      }
+    } catch (e) {
+      setIsError(true);
+      setError("Failed to pick image");
+    }
+  };
+
+  const validate = () => {
+    let valid = true;
+    let newErrors = { title: "", description: "", link: "", price: "" };
+
+    if (title.length > 70) {
+      newErrors.title = "Title must be 70 characters or less";
+      valid = false;
+    }
+
+    if (description.length > 300) {
+      newErrors.description = "Description must be 300 characters or less";
+      valid = false;
+    }
+
+    if (link.length > 300) {
+      newErrors.link = "Link must be 300 characters or less";
+      valid = false;
+    }
+
+    if (!/^\d*\.?\d+$/.test(price)) {
+      newErrors.price = "Price must be a numeric value";
+      valid = false;
+    }
+
+    setErrors(newErrors);
+    return valid;
+  };
+
+  const handleSubmit = async () => {
+    if (validate()) {
+      setIsLoading(true);
+      setIsError(false);
+      setError(null);
+
+      try {
+        let downloadURL = null;
+        if (image !== null) {
+          const imageFileName = generateRandomName() + "." + image.split(".").pop();
+          const response = await fetch(image);
+          const pictureBlob = await response.blob();
+
+          const storageRef = ref(storage, `profilePictures/${imageFileName}`);
+          await uploadBytes(storageRef, pictureBlob);
+
+          downloadURL = await getDownloadURL(storageRef);
+        }
+
+        const newPost = {
+          title,
+          description,
+          link,
+          price: parseFloat(price),
+          imageURL: downloadURL,
+          createdAt: new Date(),
+          lastUpdate: new Date(),
+          userEmail: user?.email,
+        };
+
+        await addDoc(collection(db, "Posts"), newPost); 
+        navigation.goBack();
+      } catch (e) {
+        console.error("Error uploading post:", e);
+        setIsError(true);
+        setError("Failed to upload post. Please try again later.");
+      } finally {
+        setIsLoading(false);
+      }
+    } else {
+      setIsError(true);
+      setError("Validation Error: Please correct the errors in the form.");
+    }
+  };
+
+  return {
+    title,
+    description,
+    link,
+    price,
+    image,
+    errors,
+    isLoading,
+    isError,
+    error,
+    setTitle,
+    setDescription,
+    setLink,
+    setPrice,
+    setIsError,
+    handlePickImage,
+    handleSubmit,
+  };
+};
+
+export default useAddPost;
