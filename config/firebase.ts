e4dc546c682f76499cf5c63f577c790dd0c80f35
Raz Hagay Gavriel <raz5338@cs.colman.ac.il>
2024-06-22 13:08:49 +0300

    Refactor firebase.ts to use firestore() function for database initialization

--- config/firebase.ts
+++ config/firebase.ts
@@ -1,16 +1,16 @@
 import { initializeApp } from "firebase/app";
+import firestore from "@react-native-firebase/firestore";
+import { getStorage } from "firebase/storage";
+import "@react-native-firebase/storage";
+import { firebaseConfig } from "./env";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from 'firebase/storage';
-import '@react-native-firebase/storage';
-import { firebaseConfig } from './env';
-
-
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+// export const db = firestore();
 
 export const storage = getStorage(app);
 
