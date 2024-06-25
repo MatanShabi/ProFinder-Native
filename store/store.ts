6cfca49e93abaed923cd0c3708113ecf5f66ca49
MatanShabi <46000126+MatanShabi@users.noreply.github.com>
2024-06-25 23:20:05 +0300

    config redux

---
+++ store/store.ts
@@ -0,0 +1,25 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./slices/posts";
+import { persistStore, persistReducer } from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, userReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  devTools:true,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };
