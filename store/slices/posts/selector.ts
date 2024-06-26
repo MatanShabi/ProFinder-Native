257381249623a68682029d780d90c5f22bebf638
MatanShabi <46000126+MatanShabi@users.noreply.github.com>
2024-06-26 18:54:38 +0300

    cleanup

--- store/slices/posts/selector.ts
+++ store/slices/posts/selector.ts
@@ -1,10 +1,14 @@
 import { RootState } from "@/store/store";
+import { createSelector } from "@reduxjs/toolkit";
 
-export const selectAllPosts = (state: RootState) => (
-    {
-        posts: state.posts,
-        isLoading: state.status === 'loading',
-        isError: state.error !== null,
-        error: state.error
-    })
-
+export const selectAllPosts = createSelector(
+  (state: RootState) => state.posts,
+  (state: RootState) => state.status,
+  (state: RootState) => state.error,
+  (posts, status, error) => ({
+    posts,
+    isLoading: status === 'loading',
+    isError: error !== null,
+    error
+  })
+);
